4fec15afc1d70db21b9ae98a7663964fef819b63
tammo <ta_wu@uni-bremen.de>
2018-07-23 11:25:03 +0200

    collecting method for matrix creation

--- src/image_logger.cpp
+++ src/image_logger.cpp
@@ -8,6 +8,9 @@
 
 DBClientConnection* mongodb_conn;
 
+int count = 0;
+
+
 iai_image_logging_msgs::DefaultConfig g_cfg;
 
 /**
@@ -19,25 +22,15 @@ void imageCb(sensor_msgs::ImageConstPtr msg)
 
 }
 
-/**
- * Callback for the compressed images sent by one camera
- * @param msg compressed image pointer
- */
-void compressedImageCb(sensor_msgs::CompressedImageConstPtr msg)
+void printDatabaseEntries(iai_image_logging_msgs::DefaultConfig conf, int sample_size)
 {
-  initialize();
-  BSONObjBuilder document;
-  std::string collection = g_cfg.collection;
-
-  ROS_INFO_STREAM("Saving Image at sec " << msg->header.stamp.sec << " with type " << msg->format);
-
-  Date_t stamp = msg->header.stamp.sec * 1000.0 + msg->header.stamp.nsec / 1000000.0;
-  document.append("header", BSON("seq" << msg->header.seq << "stamp" << stamp << "frame_id" << msg->header.frame_id));
-  document.append("format", msg->format);
-  document.appendBinData("data", msg->data.size(), BinDataGeneral, (&msg->data[0]));
-
-  add_meta_for_msg<sensor_msgs::CompressedImage>(msg, document);
-  mongodb_conn->insert(collection, document.obj());
+    if (count % sample_size == 0)
+    {
+        ROS_INFO_STREAM("Saved " << sample_size << " images to " << conf.collection);
+        ROS_INFO_STREAM("Format = " << conf.format);
+        ROS_INFO_STREAM("JPEG quality = " << conf.jpeg_quality);
+        ROS_INFO_STREAM("PNG level = " << conf.png_level);
+    }
 }
 
 /**
@@ -46,29 +39,122 @@ void compressedImageCb(sensor_msgs::CompressedImageConstPtr msg)
  */
 void configurationCb(iai_image_logging_msgs::DefaultConfig& cfg)
 {
-  g_cfg = cfg;
-  dynamic_reconfigure::ReconfigureRequest req;
-  dynamic_reconfigure::ReconfigureResponse res;
-  dynamic_reconfigure::StrParameter format;
-  dynamic_reconfigure::IntParameter jpeg;
-  dynamic_reconfigure::IntParameter png;
-  dynamic_reconfigure::Config conf_req;
+    g_cfg = cfg;
+    dynamic_reconfigure::ReconfigureRequest req;
+    dynamic_reconfigure::ReconfigureResponse res;
+    dynamic_reconfigure::StrParameter format;
+    dynamic_reconfigure::IntParameter jpeg;
+    dynamic_reconfigure::IntParameter png;
+    dynamic_reconfigure::Config conf_req;
 
-  format.name = "format";
-  format.value = cfg.format;
+    format.name = "format";
+    format.value = cfg.format;
 
-  jpeg.name = "jpeg_quality";
-  jpeg.value = cfg.jpeg_quality;
+    jpeg.name = "jpeg_quality";
+    jpeg.value = cfg.jpeg_quality;
 
-  png.name = "png_level";
-  png.value = cfg.png_level;
+    png.name = "png_level";
+    png.value = cfg.png_level;
 
-  req.config.strs.push_back(format);
-  req.config.ints.push_back(jpeg);
-  req.config.ints.push_back(png);
+    req.config.strs.push_back(format);
+    req.config.ints.push_back(jpeg);
+    req.config.ints.push_back(png);
 
-  ros::service::call(cfg.topic + "/compressed/set_parameters", req, res);
-  ROS_INFO_STREAM("Set parameters");
+    ros::service::call(cfg.topic + "/compressed/set_parameters", req, res);
+    ROS_DEBUG_STREAM("Set parameters on topic " << cfg.topic + "/compressed");
+    ROS_DEBUG_STREAM("Request " << req.config.ints[0].name << ": " << req.config.ints[0].value);
+    ROS_DEBUG_STREAM("Respone " << res.config.ints[0].name << ": " << res.config.ints[0].value);
+}
+
+
+void matrixFunction()
+{
+    iai_image_logging_msgs::DefaultConfig conf;
+    conf = g_cfg;
+    int sample_size = 10;
+
+    if (count < sample_size)
+    {
+        conf.format = "jpeg";
+        conf.jpeg_quality = 100;
+        conf.collection = "db.im_raw_comp_jpeg_100";
+        configurationCb(conf);
+    }
+
+    else if (count < 2* sample_size)
+    {
+        conf.format = "jpeg";
+        conf.jpeg_quality = 80;
+        conf.collection = "db.im_raw_comp_jpeg_80";
+        configurationCb(conf);
+    }
+
+    else if (count < 3 * sample_size)
+    {
+        conf.format = "jpeg";
+        conf.jpeg_quality = 30;
+        conf.collection = "db.im_raw_comp_jpeg_30";
+        configurationCb(conf);
+
+    }
+
+    else if (count < 4 * sample_size)
+    {
+        conf.format = "png";
+        conf.png_level = 1;
+        conf.collection = "db.im_raw_comp_png_1";
+        configurationCb(conf);
+
+    }
+
+    else if (count < 5 * sample_size)
+    {
+        conf.format = "png";
+        conf.png_level = 2;
+        conf.collection = "db.im_raw_comp_png_2";
+        configurationCb(conf);
+
+    }
+
+    else if (count < 6 * sample_size)
+    {
+        conf.format = "png";
+        conf.png_level = 7;
+        conf.collection = "db.im_raw_comp_png_7";
+        configurationCb(conf);
+
+    }
+
+
+    printDatabaseEntries(conf, sample_size);
+    count++;
+
+
+
+}
+/**
+ * Callback for the compressed images sent by one camera
+ * @param msg compressed image pointer
+ */
+void compressedImageCb(sensor_msgs::CompressedImageConstPtr msg)
+{
+
+
+    matrixFunction();
+
+  initialize();
+  BSONObjBuilder document;
+  std::string collection = g_cfg.collection;
+
+  ROS_DEBUG_STREAM("Saving Image at sec " << msg->header.stamp.sec << " with type " << msg->format);
+
+  Date_t stamp = msg->header.stamp.sec * 1000.0 + msg->header.stamp.nsec / 1000000.0;
+  document.append("header", BSON("seq" << msg->header.seq << "stamp" << stamp << "frame_id" << msg->header.frame_id));
+  document.append("format", msg->format);
+  document.appendBinData("data", msg->data.size(), BinDataGeneral, (&msg->data[0]));
+
+  add_meta_for_msg<sensor_msgs::CompressedImage>(msg, document);
+  mongodb_conn->insert(collection, document.obj());
 }
 
 /**
@@ -106,12 +192,14 @@ int main(int argc, char** argv)
   image_transport::TransportHints th("compressed");
   image_transport::Subscriber img_sub = it.subscribe(topic, 1, imageCb, ros::VoidPtr(), th);
 
-  ros::Subscriber sub_kinect = n.subscribe<sensor_msgs::CompressedImage>(topic + "/compressed", 1, compressedImageCb);
+  ros::Subscriber sub_kinect = n.subscribe<sensor_msgs::CompressedImage>(topic + "/compressed", 100, compressedImageCb);
 
   ros::Rate r(1.0);
 
-  while (n.ok())
+    while (n.ok())
   {
+
+
     ros::spinOnce();
     r.sleep();
   }
