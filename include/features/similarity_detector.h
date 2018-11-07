b3e5da228c41b5b9d825f80523c041242e0ae941
countdueki <tammo@wubbena.de>
2018-11-07 10:59:20 +0100

    simplified similarity detector

--- include/features/similarity_detector.h
+++ include/features/similarity_detector.h
@@ -74,44 +74,10 @@ public:
 
   bool detectMSE(sensor_msgs::CompressedImageConstPtr prev, sensor_msgs::CompressedImageConstPtr curr)
   {
-    try
-    {
-      cv::Mat prev_grey, curr_grey, diffImage;
-      double mse;
+    cv_bridge::CvImageConstPtr prev_image = cv_bridge::toCvShare(prev);
+    cv_bridge::CvImageConstPtr curr_image = cv_bridge::toCvShare(curr);
 
-      cv_bridge::CvImageConstPtr prev_image = cv_bridge::toCvCopy(prev);
-      cv_bridge::CvImageConstPtr curr_image = cv_bridge::toCvCopy(curr);
-
-      // convert to greyscale if neccessary
-
-      if (prev_image->image.channels() > 2 && curr_image->image.channels() > 2)
-      {
-        cv::cvtColor(prev_image->image, prev_grey, CV_BGR2GRAY);
-        cv::cvtColor(curr_image->image, curr_grey, CV_BGR2GRAY);
-      }
-      else
-      {
-        prev_grey = prev_image->image;
-        curr_grey = curr_image->image;
-      }
-      cv::norm(prev_grey, curr_grey, cv::NORM_L2);
-
-      cv::absdiff(prev_grey, curr_grey, diffImage);
-
-      cv::Mat square_diffImage = diffImage.mul(diffImage);
-
-      square_diffImage.convertTo(square_diffImage, CV_32F);
-      cv::Scalar sum = cv::sum(square_diffImage);
-
-      mse = (sum.val[0] + sum.val[1] + sum.val[2] + sum.val[3]) / (prev_grey.channels() * prev_grey.total());
-
-      ROS_DEBUG_STREAM("Mean Squared Error of similar images: " << mse << " (threshold: " << threshold << ")");
-      return mse < threshold;
-    }
-    catch (cv::Exception e)
-    {
-      ROS_ERROR_STREAM("Error calculating MSE: " << e.what());
-    }
+    return detectMSE(prev_image, curr_image);
   }
 };
 
