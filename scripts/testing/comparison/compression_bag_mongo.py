5f119369f17855b3b5d6c1bb1fd0b3eac7c28302
countdueki <tammo@wubbena.de>
2018-11-08 18:46:56 +0100

    runtime error_2

--- scripts/testing/comparison/compression_bag_mongo.py
+++ scripts/testing/comparison/compression_bag_mongo.py
@@ -28,28 +28,27 @@ update.quality = 34
 
 bag_duration = 10
 
-if __name__ == "__main__":
-    launcher = subprocess.Popen(["roslaunch",  "iai_image_logging", "kinect_setup.launch"])
-    time.sleep(5)
+launcher = subprocess.Popen(["roslaunch",  "iai_image_logging", "kinect_setup.launch"])
+time.sleep(5)
 
     # case 1:
 
-    rospy.wait_for_service('iai_configurator/update')
-    try:
-        insertion = rospy.ServiceProxy('iai_configurator/update', Update)
-        result = insertion(update)
-        print(result)
-    except rospy.ServiceException as e:
-        print("Service call failed: %s" % e)
+rospy.wait_for_service('iai_configurator/update')
+try:
+    insertion = rospy.ServiceProxy('iai_configurator/update', Update)
+    result = insertion(update)
+    print(result)
+except rospy.ServiceException as e:
+    print("Service call failed: %s" % e)
 
     #insert_1 = subprocess.Popen(["rosservice", "call", "/iai_configurator/update",
     #                             "{'topic':'" + topic + "', 'iai_id':'"+topic_name + "', 'format':'" + format + "' , 'jpeg_quality': " + str(jpeg_quality) + ", 'collection':'db." + collection + "' , 'mode':'" + mode + "', 'db_host':'localhost'}"])
     #rosbag_1 =  subprocess.Popen(["rosbag",  "record", topic, "-O", collection,  "--duration=" + str(bag_duration)])
-    time.sleep(bag_duration)
+time.sleep(bag_duration)
 
     #os.kill(insert_1.pid, signal.SIGTERM)
     #os.kill(rosbag_1.pid, signal.SIGTERM)
-    os.kill(launcher.pid, signal.SIGTERM)
-    sys.exit(1)
+os.kill(launcher.pid, signal.SIGTERM)
+sys.exit(1)
 
     # rosrun dynamic_reconfigure dynparam set /iai_updater "{'topic':'/camera/rgb/image_raw', 'format':'jpeg', 'jpeg_quality':100, 'png_level':9, 'db_host':'localhost', 'collection':'db.standard','optimize_for': 0, 'target_bitrate': 800000, 'keyframe_frequency':64, 'quality': 31, 'depth_max':10.0, 'depth_quantization':100.0, 'mode':0, 'cam_no':0}"
